c30a0798228d8689fb20ea52b26abf41e525d307
Andre Furlan <ensismoebius@gmail.com>
2023-07-10 09:44:48 -0300

    Open MPI version implemented

--- code2/IntegralEvaluator/main.cpp
+++ code2/IntegralEvaluator/main.cpp
@@ -4,7 +4,7 @@
 #include <mpi.h>
 
 /*
- * Inlining functions so the call overhead
+ * Inlinning functions so the call overhead
  * is avoided
  */
 inline long double f(long double x)
@@ -13,9 +13,9 @@ inline long double f(long double x)
 }
 
 /*
- * Inlining the functions so the call overhead
- * is avoided. Note that **inline** is not a
- * command; the compiler may **ignore** the inline
+ * Inlinning the functions so the call overhead
+ * is avoided. Note that **inline** it is not a
+ * command, the compiler may **ignore** the inline
  * request.
  */
 inline long double integrate(long double (*f)(long double),
@@ -35,45 +35,54 @@ inline long double integrate(long double (*f)(long double),
     return (h / 2) * sum;
 }
 
-inline void experiment(
-    double lowerLimit, double upperLimit, double interval, int myRank, int numProcs)
+inline void experiment(double lowerLimit, double upperLimit, double interval)
 {
     long double result = 0;
-    int partitions = static_cast<int>(upperLimit / interval);
-    int localPartitions = partitions / numProcs;
-    int localStart = myRank * localPartitions;
-    int localEnd = localStart + localPartitions;
+    int partitions = upperLimit / interval;
 
-    long double localSum = integrate(f,
-                                     lowerLimit + localStart * interval,
-                                     lowerLimit + localEnd * interval,
-                                     localPartitions);
+    double start = MPI_Wtime();
+    result = integrate(f, lowerLimit, upperLimit, partitions);
+    double end = MPI_Wtime();
 
-    // Gather results from all processes
-    MPI_Reduce(&localSum, &result, 1, MPI_LONG_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    // Calculating total time taken by the program.
+    double timeTaken = end - start;
 
-    if (myRank == 0) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    long double *allResults = nullptr;
+
+    if (rank == 0) {
+        allResults = new long double[size];
+    }
+
+    MPI_Gather(&result, 1, MPI_LONG_DOUBLE, allResults, 1, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
         std::cout << std::fixed << std::setprecision(6) << interval << " \t " << partitions
-                  << " \t " << result << std::endl;
+                  << " \t " << timeTaken << " \t " << allResults[0] << std::endl;
+        delete[] allResults;
     }
 }
 
 int main(int argc, char **argv)
 {
-    double lowerLimit = 0, upperLimit = 100;
-    int numProcs, myRank;
-
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
-    if (myRank == 0) {
-        std::cout << "Interval \t Partitions \t Result" << std::endl;
+    double lowerLimit = 0, upperLimit = 100;
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        std::cout << "Interval \t Partitions \t Time taken \t Result" << std::endl;
     }
 
-    experiment(lowerLimit, upperLimit, 0.000001, myRank, numProcs);
-    experiment(lowerLimit, upperLimit, 0.00001, myRank, numProcs);
-    experiment(lowerLimit, upperLimit, 0.0001, myRank, numProcs);
+    experiment(lowerLimit, upperLimit, 0.000001);
+    experiment(lowerLimit, upperLimit, 0.00001);
+    experiment(lowerLimit, upperLimit, 0.0001);
 
     MPI_Finalize();
 
