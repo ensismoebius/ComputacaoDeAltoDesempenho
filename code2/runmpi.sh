570315e096526ea1a6cb5256dcbd48241cfa3554
Andre Furlan <ensismoebius@gmail.com>
2023-07-11 20:55:22 -0300

    Open MPI part is done!

--- code2/runmpi.sh
+++ code2/runmpi.sh
@@ -1,2 +1,2 @@
 # Change "build-IntegralEvaluator-Desktop-Release/parallel_integration" to the path of your open MPI executable
-mpirun --hostfile hosts.txt -np 10 ./build-IntegralEvaluator-Desktop-Release/parallel_integration
+mpirun --hostfile hosts.txt -np 1 ./build-IntegralEvaluator-Desktop-Release/parallel_integration
